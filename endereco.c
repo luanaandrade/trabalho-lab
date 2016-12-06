a800237a9a3add5fe2fd624e026df49a3f7e07de
Luana Andrade <luanaandrade.sm@gmail.com>
2016-12-06 09:39:39 -0400

    Create endereco.c

---
+++ endereco.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 10;		//Suponha que esta variável está alocada a partir do byte "62288"
+	char c = 'a';	//Suponha que esta variável está alocada a partir do byte "62289"
+	
+	printf("%d", i);	//Imprime "10"
+	printf("%d", &i);	//Imprime "62288"
+	printf("%c", c);	//Imprime "a"
+	printf("%c", &c);	//Imprime "62289"
+}
